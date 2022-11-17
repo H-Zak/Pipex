90ec48e927ac0c3ac6fa124bd874d1610a4201fa
Zakariya HAMDOUCHI <zhamdouc@e3r8p13.clusters.42paris.fr>
2022-11-17 19:18:50 +0100

    new

---
+++ includes/pipex.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zhamdouc <zhamdouc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 19:14:43 by zhamdouc          #+#    #+#             */
+/*   Updated: 2022/11/17 19:15:40 by zhamdouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <unistd.h>
+# include "./../libs/printf/libftprintf.h"
+# include "./../libs/gnl/get_next_line.h"
+
+#endif
